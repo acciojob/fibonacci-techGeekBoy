fc066f62eeb6e76b0dd2622c3fa11c6c5dd616f2
techGeekBoy <148032840+techGeekBoy@users.noreply.github.com>
2024-02-08 18:37:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function fibonacci(num) {
-// your code here
-	let Arr = [0,1];
-	for(let i=2; i<=num; i++){
-		Arr.push(Arr[i-1] + Arr[i-2])
-	};
-	return Arr;
+function fibonacciLoop(n) {
+    let fibArray = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+    }
+    return fibArray;
 }
 
-module.exports = fibonacci;
+// Example usage:
+console.log(fibonacciLoop(10)); // Outputs: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
