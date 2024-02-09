c4556c3452c5ce11a2d85cd7ac200cbbdd4c3a36
techGeekBoy <148032840+techGeekBoy@users.noreply.github.com>
2024-02-09 11:31:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function fibonacciLoop(n) {
-    let fibArray = [0, 1];
-    for (let i = 2; i < n; i++) {
-        fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
-    }
-    return fibArray[n-1];
+function fibonacci(num) {
+	if (num === 1) return 0;
+	else if( num === 2) return 1;
+	num -= 2;
+	var num1 =0;
+	var num2 = 1;
+	var sum;
+	var i =0;
+	for(i =0; i< num ; i++){
+	sum = num1 + num2;
+		num1 = num2;
+		num2 = sum;
+	}
+	return num2;
 }
+
+module.exports = fibonacci;
+//     let fibArray = [0, 1];
+//     for (let i = 2; i < n; i++) {
+//         fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+//     }
+//     return fibArray[n-1];
+// }
 
